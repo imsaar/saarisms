b3fa6d6d98de09ebc6a9461c294991bee859626e
Ali Rizvi <aliabbasrizvi@gmail.com>
2010-07-21 06:38:37 -0700

    adding smart image rotation script from blog

---
+++ rotate_smart.rb
@@ -0,0 +1,32 @@
+require 'rmagick'
+require 'exifr'
+require 'pp'
+
+ROTATE_CLOCKWISE = 90
+ROTATE_ANTICLOCKWISE = -90
+
+def rotate(file)
+  image_exif = EXIFR::JPEG.new(file).exif
+
+  puts "#{file} : #{image_exif[:orientation]}"
+  if (image_exif[:orientation] == EXIFR::TIFF::RightTopOrientation)
+    degrees = ROTATE_CLOCKWISE
+  elsif (image_exif[:orientation] == EXIFR::TIFF::LeftBottomOrientation)
+    degrees = ROTATE_ANTICLOCKWISE
+  else
+    puts "Not rotating #{file}"
+    return
+  end
+
+  image = (Magick::Image.read(file)).first
+  #image.rotate(degrees).write("#{file.split('.').first}_rotated.jpg")
+  image.rotate(degrees).write("#{file}")
+end
+
+dir = ARGV[0]
+
+Dir.entries(dir).each do |file|
+  if (file =~ /jpg/i)
+    rotate(File.join(dir, file))
+  end
+end
