51df463a0866df60ecf0fbcadc1c590601f504d6
Ali Rizvi <arizvi@avvo.com>
2020-11-06 10:45:32 -0800

    add command and update help

--- utilities/automator/cmd.rb
+++ utilities/automator/cmd.rb
@@ -1,35 +1,50 @@
+# AppleScript
+# on run {input, parameters}
+#
+#       activate me
+#
+#       return input
+#end run
+
+# Ask for Text
+# a = attorney profile | adp = avvo adp | g = github/avvo | j = jira | ch = changelog | hb = honeybadger | pd = pagerduty | sh = shipit | csmr 20 = 20% time doc  | csmr tt = ticket template
+
 input = STDIN.gets.chomp.strip
 cmd, param = input.split
 
 case cmd 
 when 'g'
  repo = case param
-		 when 'a'
-		 	'amos'
-		 when 'p'
-			'attorney_profiles'
-		 when 'c'
-			'avvo_common'
-		 when 's'
-			'solicitor'
-	    end
+        when 'a'
+          'amos'
+        when 'p'
+          'attorney_profiles'
+        when 'c'
+          'avvo_common'
+        when 's'
+          'solicitor'
+        else
+          param
+        end
  `open "http://github.com/avvo/#{repo}"`
 when 'j'
- `open https://jira.internetbrands.com/browse/#{param}`
+  `open https://jira.internetbrands.com/browse/#{param}`
 when 'adp'
   `open https://workforcenow.adp.com/`
 when 'sh'
-	`open http://ship-it.corp.avvo.com/`
+  `open http://ship-it.corp.avvo.com/`
 when 'ch'
-	`open https://changelog.avvo.com/`
+  `open https://changelog.avvo.com/`
 when 'pd'
-	`open https://avvo.pagerduty.com/incidents`
+  `open https://avvo.pagerduty.com/incidents`
 when 'hb'
-	`open https://app.honeybadger.io/`
+  `open https://app.honeybadger.io/`
 when 'a'
-	`open "https://www.avvo.com/attorneys/#{param}"`
-when '20'
-	`open https://docs.google.com/spreadsheets/d/15jqbHcr-t5fomFiHziuUkJC-563T9AnHsL5sFE6OGJw/edit#gid=2121409843`
+  `open "https://www.avvo.com/attorneys/#{param}"`
+when 'csmr 20'
+  `open https://docs.google.com/spreadsheets/d/15jqbHcr-t5fomFiHziuUkJC-563T9AnHsL5sFE6OGJw/edit#gid=2121409843`
+when 'csmr tt'
+  `open https://confluence.internetbrands.com/pages/viewpage.action?spaceKey=AVVO&title=Consumer+Team+Ticket+Template`
 else
- ""
+  "http://google.com/search?q=#{param}"
 end
